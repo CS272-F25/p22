33bff9ed25811e5a8053c2e136c0475041175b5d
pethad <pethad@wisc.edu>
2025-11-21 09:45:14 -0600

    added api constants

---
+++ api_constants.js
@@ -0,0 +1,19 @@
+// API URL constants for Smart Sausage
+
+// Base API endpoint for Open Trivia DB
+export const OTDB_BASE = "https://opentdb.com/api.php";
+
+// Category ID map (easy to reference later)
+export const CATEGORY = {
+  SCIENCE_NATURE: 17,
+  MATH: 19,
+  GEOGRAPHY: 22,
+  HISTORY: 23,
+  ART: 25
+};
+
+export const API_URLS = {
+  MATH_10: "https://opentdb.com/api.php?amount=10&category=19",
+  GEOGRAPHY_10: "https://opentdb.com/api.php?amount=10&category=22",
+  ART_10: "https://opentdb.com/api.php?amount=10&category=25"
+};
